b87d8536b51ac0a8315bc5297777804e376c2bcf
giuliogallo94 <giuliogallo1994@gmail.com>
2024-02-05 16:43:31 +0100

    add: routes for single project

--- src/pages/AppDetailsProject.vue
+++ src/pages/AppDetailsProject.vue
@@ -1,5 +1,11 @@
 <script></script>
 
-<template>Ciao</template>
+<template>
+  <div class="container">
+    <h1>Project Title</h1>
+
+    <p><b>Creation date:</b></p>
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
--- src/router/router.js
+++ src/router/router.js
@@ -18,7 +18,7 @@ const router = createRouter({
       component: AppProjects,
     },
     {
-      path: "/details",
+      path: "/projects/:slug",
       name: "details-project",
       component: AppDetailsProject,
     },
